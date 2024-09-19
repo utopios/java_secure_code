ae4fc1c3fcafae5b8d0783eec3c4a1d57b24491b
ihab abadi <ihab@utopios.net>
2024-09-19 10:17:14 +0200

    validation de format

--- demo_jour_2/src/main/java/com/example/demo_jour_2/controller/DataController.java
+++ demo_jour_2/src/main/java/com/example/demo_jour_2/controller/DataController.java
@@ -4,6 +4,7 @@ package com.example.demo_jour_2.controller;
 import com.example.demo_jour_2.dto.DataRequest;
 import com.example.demo_jour_2.dto.DataResponse;
 import jakarta.validation.Valid;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataController {
     public ResponseEntity<DataResponse> post(@Valid @RequestBody DataRequest dataRequest) {
         return ResponseEntity.ok(new DataResponse());
+    }
+
+    public ResponseEntity<String> get() {
+        String content = "<html><p></p></html>";
+        return ResponseEntity.ok(StringEscapeUtils.escapeHtml4(content));
     }
 }
